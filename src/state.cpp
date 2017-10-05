136585c66e5e68a30f4a06be05e25cd1cdab7dbd
Harry Kalogirou <harkal@gmail.com>
2017-10-05 16:06:19 +0300

    state compile, state work

--- src/state.cpp
+++ src/state.cpp
@@ -1,6 +1,21 @@
 #include "state.h"
 
-CState::CState()
+CState::CState(const boost::filesystem::path& path) : mDB(path, 1 << 22), mState(&mDB)
 {
 
+}
+
+void CState::reset()
+{
+    mState.init();
+}
+
+void CState::SetRoot(const H256& root)
+{
+    mState.SetRoot(root);
+}
+
+bool CState::IsAddressInUse(const CPubKey& address) const
+{
+    return mState.Contains(address);
 }
--- src/state.h
+++ src/state.h
@@ -1,11 +1,23 @@
 #ifndef STATE_H
 #define STATE_H
 
+#include <boost/filesystem.hpp>
+#include "dbwrapper.h"
+#include "triedb/triedb.h"
+#include "key.h"
 
 class CState
 {
 public:
-    CState();
+    CState(const boost::filesystem::path& path);
+
+    void reset();
+
+    void SetRoot(const H256& root);
+    bool IsAddressInUse(const CPubKey& address) const;
+private:
+    CDBWrapper *mDB = 0;
+    CTrieDB<CDBWrapper> *mState = 0;
 };
 
 #endif // STATE_H
